cc85452ceaaa07b16c3c3b655267cb949baba7ee
Matthieu Devallé <matthieu@MacBook-Pro-de-Matthieu.local>
2024-05-21 12:07:44 +0200

    nouvelle version

---
+++ Site/_data/metadata.js
@@ -0,0 +1,29 @@
+module.exports = {
+    title: "Portail du BARRI",
+    url: "https://cisirh.github.io",
+    newsletter: [
+        {
+            url: "",
+            title: "",
+            description: ""
+        }
+    ],
+    facebook_url: "",
+    mastodon_url: "",
+    twitter_url: "https://x.com/cisirh",
+    instagram_url: "",
+    linkedin_url: "https://www.linkedin.com/company/cisirh",
+    youtube_url: "https://www.youtube.com/channel/UCOG65O7ph52p1iCXr05pQAw",
+    peertube_url: "",
+    github_url: "https://github.com/CISIRH",
+    sourcehut_url: "",
+    repository_url: "https://github.com/CISIRH/portail-barri",
+    language: "fr",
+    description: "Centre interministériel de services informatiques relatifs aux ressources humaines (CISIRH)",
+    author: {
+        name: "Centre interministériel de services informatiques relatifs aux ressources humaines",
+        email: "contact.cisirh@finances.gouv.fr",
+        url: "https://www.economie.gouv.fr/cisirh"
+    },
+    service: "Ministère de la Transformation et de la Fonction Publiques"
+}
---
+++ Site/content/fr/veille-reglementaire/index.md
@@ -0,0 +1,13 @@
+---
+layout: layouts/page.njk
+title: Veille réglementaire
+eleventyNavigation:
+  key: Veille réglementaire
+  parent:
+  order: 4
+---
+
+La veille réglementaire recense les différents textes publiés au JORF ayant un impact sur les référentiels de l’offre OPEN RH FPE (Noyau, RCC, RDP, Bibliothèque des actes). Elle détermine, pour les ministères, les textes ayant un impact sur les référentiels de l’offre gérés dans leur SIRH.
+
+Cette veille s’appuie sur la veille quotidienne réalisée par la DGAFP et sur l’analyse mensuelle du Bureau CE-2A de la DGFiP qui identifie spécifiquement les textes ayant un impact en paie.
+
